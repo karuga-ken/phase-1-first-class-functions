47da5dabf4a2ad5048b6a1a3c02f044b43ed86c9
Karuga Mbugua <karugambugua@icloud.com>
2023-10-05 15:13:49 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(kenn){
+    kenn();
+}
+
+function returnsANamedFunction(){
+    return function ken(){
+        
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
